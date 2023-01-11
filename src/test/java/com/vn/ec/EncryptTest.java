0ec1831ae15ede7b2073f8b288509a38abf0e2be
alo <kusodaima3@gmail.com>
2023-01-11 21:48:12 +0700

    signUp

--- src/test/java/com/vn/ec/EncryptTest.java
+++ src/test/java/com/vn/ec/EncryptTest.java
@@ -14,7 +14,9 @@ public class EncryptTest {
     void t() {
         var pass = "12345";
         var passNew = EncryptAndDecrypt.encrypt(pass.getBytes(StandardCharsets.UTF_8), Constants.PASSWORD_AIR_WATER_DE);
+        System.out.println(passNew);
 
         var password = EncryptAndDecrypt.decrypt(passNew, Constants.PASSWORD_AIR_WATER_DE);
+        System.out.println(password);
     }
 }
